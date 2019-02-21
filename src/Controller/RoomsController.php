c24244f7becf51e2fa1a488463b438fe35b218f5
Paul-DT <paul-david_traore@hotmail.com>
2019-02-21 03:05:29 -0500

    rooms index return archived rooms and rooms in 2 different array

--- src/Controller/RoomsController.php
+++ src/Controller/RoomsController.php
@@ -20,9 +20,12 @@ class RoomsController extends AppController
      */
     public function index()
     {
-        $rooms = $this->paginate($this->Rooms);
-
+        $rooms = $this->Rooms->find('all')->where(['deleted IS' => null])->orWhere(['deleted IS' => ""])->order(['name' => 'asc']);
+        $archivedRooms = $this->Rooms->find('all')->where(['deleted IS NOT' => ""])->order(['name' => 'asc']);
         $this->set(compact('rooms'));
+        $this->set('_serialize', ['rooms']);
+        $this->set(compact('archivedRooms'));
+        $this->set('_serialize', ['archivedRooms']);
     }
 
     /**
