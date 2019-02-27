af4e0e280c5f0883ba803efd8a10d1bdc4de575b
david <david.lafreniere@live.ca>
2019-02-27 10:27:39 -0500

    Made sort for mentors report (server-side)

--- src/Controller/ReportsController.php
+++ src/Controller/ReportsController.php
@@ -18,23 +18,17 @@ class ReportsController extends AppController
      */
     public function index()
     {
-        /*ini_set('memory_limit', '-1');
-        $licences = $this->Licences->find('all', [
-            'contain' => ['Products']
-        ])->where('deleted is null')->order(['name' => 'asc']);
-        $archivedLicences = $this->Licences->find('all', [
-            'contain' => ['Products']
-        ])->where('deleted is not null')->order(['name' => 'asc']);
-        $this->set(compact('licences', 'archivedLicences'));
-        $this->set('_serialize', ['licences', 'archivedLicences']);*/
     }
 
     public function mentorsReport()
     {
         $start_date = $this->getRequest()->getQuery('start_date');
         $end_date = $this->getRequest()->getQuery('end_date');
+        $sort_field = $this->getRequest()->getQuery('sort_field') != null ? $this->getRequest()->getQuery('sort_field') : 'email';
+        $sort_dir = $this->getRequest()->getQuery('sort_dir') != null ? $this->getRequest()->getQuery('sort_dir') : 'asc';
+
         $conn = ConnectionManager::get('default');
-        $proc_result = $conn->execute("call mentors_report(?,?)", [$start_date, $end_date])->fetchAll('assoc');
+        $proc_result = $conn->execute("call mentors_report(?,?,?,?)", [$start_date, $end_date, $sort_field, $sort_dir])->fetchAll('assoc');
 
         $this->set('report', $proc_result);
         $this->set('_serialize', 'report');
